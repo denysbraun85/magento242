8e8afbee54f5aebc194d7060626cc25b33fe958c
Denys <denis.sakhniuk@qlicks.nl>
2021-04-21 14:28:32 +0300

    task solution 1.5

--- app/code/Training/TestOM/Model/Test.php
+++ app/code/Training/TestOM/Model/Test.php
@@ -3,31 +3,33 @@ namespace Training\TestOM\Model;
 
 class Test
 {
-    private $manager;
     private $arrayList;
     private $name;
     private $number;
+    private $managerFactory;
 
     public function __construct(
-        ManagerInterface $manager,
         string $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        ManagerInterfaceFactory $managerFactory
     ) {
-        $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
+        $this->managerFactory = $managerFactory;
     }
 
     public function log()
     {
-        print_r(get_class($this->manager));
         echo '<br>';
         print_r($this->name);
         echo '<br>';
         print_r($this->number);
         echo '<br>';
         print_r($this->arrayList);
+        echo '<br>';
+        $newManager = $this->managerFactory->create();
+        print_r(get_class($newManager));
     }
 }
