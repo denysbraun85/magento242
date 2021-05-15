4e4596040db08a054fec4368bc45cca9c4d20acf
Denys <denis.sakhniuk@qlicks.nl>
2021-05-16 01:04:13 +0300

    task solution 2.4 (end)

--- app/code/Training/Test/Controller/Index/Index.php
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -3,11 +3,23 @@
 namespace Training\Test\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\Raw;
 
 class Index extends Action
 {
+    private $resultRaw;
+
+    public function __construct(
+        Context $context,
+        Raw $resultRaw
+    ) {
+        $this->resultRaw = $resultRaw;
+        parent::__construct($context);
+    }
     public function execute()
-{
-    $this->getResponse()->appendBody('simple text');
-}
+    {
+        $resultRaw = $this->resultRaw->create();
+        $resultRaw->setContents('simple text');
+    }
 }
